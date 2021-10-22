18eee4c9cdf41104a9177478b158b3303d816413
Peter St. John <peterc.stjohn@gmail.com>
2021-10-22 08:31:16 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Data and trained models for predicting the stability and redox potentials of organic radicals
+
+Part of an upcoming work on optimizing organic radical scaffolds for aqueous redox flow batteries.
+
+* `data/radical_db_redox_potentials.csv.gz` contains the redox potentials (in V relative to SHE) calculated with Guassian 16c at the  M06-2x/def2-tzvp level of theory in an implicit water solvent
+* `data/radical_db_spins_and_bv.csv.gz` contains the parsed electron spins, normalized 'fractional spins' (ensuring all spin values are positive and sum to 1.0, assuming hydrogen spins are zero), and calculated buried volumes at a 3.5 Angstrom radius for all heavy atoms in the dataset. Atom index corresponds to the atom index assigned by RDKit after parsing the SMILES string.
+* `models/redox_model/` contains the trained weights for a GNN that predicts the oxidation and reduction potentials of a given radical input
+* `models/stability_model/` predicts the fractional spin and buried volumes on each atom in the given input radical, and can be used to derive the combined stability score.
+
+
---
+++ models/preprocessor.py
@@ -0,0 +1,51 @@
+import os
+from typing import Optional
+
+import rdkit
+from nfp.preprocessing.features import get_ring_size
+from nfp.preprocessing.mol_preprocessor import MolPreprocessor
+
+
+def atom_featurizer(atom: rdkit.Chem.Atom) -> str:
+    """ Return an string representing the atom type
+    :param atom: the rdkit.Atom object  
+    :return: a string representation for embedding
+    """
+
+    return str((atom.GetSymbol(), atom.GetNumRadicalElectrons(), atom.GetFormalCharge(), atom.GetChiralTag().name,
+                atom.GetIsAromatic(), get_ring_size(atom, max_size=6), atom.GetDegree(),
+                atom.GetTotalNumHs(includeNeighbors=True)))
+
+
+def bond_featurizer(bond: rdkit.Chem.Bond, flipped: bool = False) -> str:
+    """Return a string representation of the given bond
+
+    :param bond: The rdkit bond object
+    :param flipped: Whether the bond is considered in the forward or reverse direction
+    :return: a string representation of the bond type
+    """
+    if not flipped:
+        atoms = "{}-{}".format(*tuple((bond.GetBeginAtom().GetSymbol(), bond.GetEndAtom().GetSymbol())))
+    else:
+        atoms = "{}-{}".format(*tuple((bond.GetEndAtom().GetSymbol(), bond.GetBeginAtom().GetSymbol())))
+
+    bstereo = bond.GetStereo().name
+    btype = str(bond.GetBondType())
+    ring = 'R{}'.format(get_ring_size(bond, max_size=6)) if bond.IsInRing() else ''
+
+    return " ".join([atoms, btype, ring, bstereo]).strip()
+
+
+def load_preprocessor(saved_preprocessor_file: Optional[str] = None) -> MolPreprocessor:
+    """Load the MolPreprocessor object from either the default json file or a provided data file
+
+    :param saved_preprocessor_file: directory of the saved nfp.Preprocessor json data
+    :return: a MolPreprocessor instance for the molecule policy network
+    """
+    preprocessor = MolPreprocessor(atom_features=atom_featurizer, bond_features=bond_featurizer, output_dtype='int64')
+
+    if not saved_preprocessor_file:
+        saved_preprocessor_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'preprocessor.json')
+
+    preprocessor.from_json(saved_preprocessor_file)
+    return preprocessor
